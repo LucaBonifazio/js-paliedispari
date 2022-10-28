873adfd8f15d1403d50932cef674b99ac742219c
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-28 16:48:54 +0200

    Work on step 2

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 const userWord = prompt('Inserisci una parola');
+console.log(userWord);
 
 if (palindrome(userWord)) {
-	console.log('è palindroma');
+    console.log('è palindroma');
+    alert('è palindroma');
 } else {
 	console.log('Non è palindroma');
+    alert('Non è palindroma');
 }
 
 function palindrome(str) {
@@ -20,12 +23,6 @@ function palindrome(str) {
     let reverseStr = lowRegStr.split('').reverse().join(''); 
     return reverseStr === lowRegStr;
   }
- 
-
-
-
-
-
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -33,3 +30,37 @@ function palindrome(str) {
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+const userChoice = prompt('Scegli, pari o dispari?');
+console.log(userChoice);
+
+if (userChoice.value !== 'pari' && 'dispari') {
+    alert('Valore errato!');
+    console.log('Valore errato!');
+}
+
+const userChoiceNumber = parseInt(prompt('Scegli un numero da 1 a 5?'));
+console.log(userChoiceNumber);
+
+if (userChoiceNumber > 5) {
+    alert('Scelta inopportuna!');
+    console.log('Scelta inopportuna!');
+}
+
+if (isEven(userChoiceNumber)) {
+    console.log('Hai vinto!');
+} else {
+    console.log('Hai perso!');
+}
+
+function isEven(number, number2) {
+    if (number % 2) {
+        return false;
+	} else {
+        return true;
+	}
+}
+
+function botChoice(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
