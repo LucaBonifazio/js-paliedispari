bf9791e3a01fc5e3fc032839971a540ab55a47ad
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-28 15:29:26 +0200

    First step

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
 // Palindroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const userWord = prompt('Inserisci una parola');
+
+if (palindrome(userWord)) {
+	console.log('è palindroma');
+} else {
+	console.log('Non è palindroma');
+}
+
+function palindrome(str) {
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
+ 
+
+
+
+
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
-// Consigli del giorno
-// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
