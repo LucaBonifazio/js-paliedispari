108ca653748b901d2e9d78b5e69359701815e274
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-02 11:44:47 +0100

    fix error

--- js/script.js
+++ js/script.js
@@ -17,44 +17,69 @@ if (palindrome(userWord)) {
 }
 
 // Creare una funzione per capire se la parola inserita è palindroma
+
 function palindrome(str) {
-    let re = /[\W_]/g;
-    let lowRegStr = str.toLowerCase().replace(re, '');
-    let reverseStr = lowRegStr.split('').reverse().join(''); 
+    const re = /[\W_]/g;
+    const lowRegStr = str.toLowerCase().replace(re, '');
+    const reverseStr = lowRegStr.split('').reverse().join(''); 
     return reverseStr === lowRegStr;
-  }
 
-
+    // metodo 2
+    // let invertedWord = '';
+    // for (let i = testWord.length - 1; i >= 0; i--) {
+      // 	invertedWord += testWord[i];
+      // }
+      // if (testWord === invertedWord) {
+        // 	return true;
+        // }
+	// return false;
+  
+	// metodo 3
+	// const indexHalf = parseInt(testWord.length / 2);
+	// for (let i = 0; i < indexHalf; i++) {
+    // 	console.log(testWord[i], testWord[testWord.length - 1 - i]);
+    // 	if (testWord[i] !== testWord[testWord.length - 1 - i]) {
+      // 		return false;
+      // 	}
+      // }
+      // return true;
+    }
 
 
 // Pari e Dispari
 
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const userChoice = prompt('Scegli, pari o dispari?');
-console.log(userChoice);
+let userChoice = prompt('Scegli, pari o dispari?').toLowerCase();;
+userChoice = userChoice === 'pari' ? 0 : 1;
+// console.log(userChoice);
 const userChoiceNumber = parseInt(prompt('Scegli un numero da 1 a 5?'));
-console.log(userChoiceNumber);
-
-// Sommiamo i due numeri
-const result = (userChoice + botChoice);
+// console.log(userChoiceNumber);
+const botChoice = getRandomInteger(1, 5);
 
 // Dichiariamo chi ha vinto.
-if (isEven(result)) {
-    console.log('Hai vinto!');
+if (isDivisible(userChoiceNumber + botChoice, userChoice)) {
+	console.log(userChoiceNumber, botChoice, 'Hai vinto');
 } else {
-    console.log('Hai perso!');
+	console.log(userChoiceNumber, botChoice, 'Hai perso');
 }
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function botChoice(min, max) {
+function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function isEven(number) {
     if (number % 2) {
-        return false;
+      return false;
 	} else {
-        return true;
+      return true;
 	}
+}
+
+function isDivisible(number, divisibility) {
+	if ((divisibility === 0 && number % 2 === 0) || (divisibility === 1 && number % 2 !== 0)) {
+		return true;
+	}
+	return false;
 }
